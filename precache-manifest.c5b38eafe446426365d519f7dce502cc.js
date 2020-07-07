5e16d4e4c250bd00ad297ba0e9547edd635dfc8b
abidh0601 <abidh0601@gmail.com>
2020-07-07 11:51:31 -0400

    Updates

---
+++ precache-manifest.c5b38eafe446426365d519f7dce502cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87e1483ca0d576eae86e65688fef84e2",
+    "url": "/bank-of-react/index.html"
+  },
+  {
+    "revision": "f8b5872169e6dbaa5030",
+    "url": "/bank-of-react/static/css/main.a5993d26.chunk.css"
+  },
+  {
+    "revision": "5d21a88b74a41722a2b4",
+    "url": "/bank-of-react/static/js/2.a14825f7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bank-of-react/static/js/2.a14825f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8b5872169e6dbaa5030",
+    "url": "/bank-of-react/static/js/main.0dd2bb55.chunk.js"
+  },
+  {
+    "revision": "c51dbc74d5655155a12e",
+    "url": "/bank-of-react/static/js/runtime-main.3c3b4ec9.js"
+  }
+]);
